414b71db7b4c246946c232b808d054664782491b
Kia Shakiba <kiashakiba@protonmail.com>
2023-07-22 00:38:50 -0400

    Fix rust tab issue

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,14 @@ require("theme")
 
 vim.opt.number = true
 vim.opt.cursorline = true
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.g.rust_recommended_style = false
+
 vim.opt.so = 999
 vim.opt.completeopt = "menuone,noinsert,noselect"
 vim.opt.wrap = false
