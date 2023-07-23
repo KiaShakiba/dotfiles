7888caa9a38049a9b41edca09939be2bb4ccdc55
Kia Shakiba <kiashakiba@protonmail.com>
2023-07-23 13:52:35 -0400

    Update plugin lua scripts

---
+++ nvim/after/plugin/tree.lua
@@ -0,0 +1,33 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+local function handle_attach(bufnr)
+	local api = require('nvim-tree.api')
+
+	local function opts(desc)
+		return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	api.config.mappings.default_on_attach(bufnr)
+
+	vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'));
+
+end
+
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	on_attach = handle_attach,
+})
+
+vim.cmd("autocmd VimEnter * NvimTreeOpen")
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,13 @@
 require("plugins")
-require("tree")
-require("theme")
 
 vim.opt.number = true
 vim.opt.cursorline = true
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = false
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.g.rust_recommended_style = false
-
 vim.opt.so = 999
 vim.opt.completeopt = "menuone,noinsert,noselect"
 vim.opt.wrap = false
 vim.opt.mouse = ""
 
-vim.keymap.set("n", "<esc>", ":noh<return><esc>")
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
