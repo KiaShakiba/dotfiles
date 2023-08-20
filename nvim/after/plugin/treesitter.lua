6b276ae3a314f1074b637d26f393a29a4ba7c870
Kia Shakiba <kiashakiba@protonmail.com>
2023-08-20 15:15:02 -0400

    Disable treesitter for cpp

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,12 +1,12 @@
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "typescript" },
+	ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "javascript", "typescript" },
 
 	sync_install = false,
 	auto_install = true,
 
 	highlight = {
 		enable = true,
-		disable = { "c", "rust" },
+		disable = { "c", "cpp", "rust" },
 		additional_vim_regex_highlighting = false,
 	},
 }
