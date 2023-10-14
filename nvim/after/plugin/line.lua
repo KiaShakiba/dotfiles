376506ee8bd459a4b4a76c8b7eb5719a334dad2b
Kia Shakiba <kiashakiba@protonmail.com>
2023-10-14 19:00:41 -0400

    Fix no lsp bug

--- nvim/after/plugin/line.lua
+++ nvim/after/plugin/line.lua
@@ -86,8 +86,22 @@ local conditions = {
 	end,
 
 	has_lsp = function()
+		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
 		local clients = vim.lsp.get_active_clients()
-		return next(clients) ~= nil
+
+		if next(clients) == nil then
+			return false
+		end
+
+		for _, client in ipairs(clients) do
+			local filetypes = client.config.filetypes
+
+			if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+				return true
+			end
+		end
+
+		return false
 	end,
 }
 
