8ab794a818298af5c415d6245985639d18bb9318
Kia Shakiba <kiashakiba@protonmail.com>
2023-10-17 12:04:51 -0400

    Update mode colours

--- nvim/after/plugin/line.lua
+++ nvim/after/plugin/line.lua
@@ -16,27 +16,27 @@ local colors = {
 }
 
 local mode_colors = {
-		n = colors.red,
-		i = colors.green,
-		v = colors.blue,
-		[""] = colors.blue,
-		V = colors.blue,
-		c = colors.magenta,
-		no = colors.red,
-		s = colors.orange,
-		S = colors.orange,
-		[""] = colors.orange,
-		ic = colors.yellow,
-		R = colors.violet,
-		Rv = colors.violet,
-		cv = colors.red,
-		ce = colors.red,
-		r = colors.cyan,
-		rm = colors.cyan,
-		["r?"] = colors.cyan,
-		["!"] = colors.red,
-		t = colors.red,
-	}
+	n = colors.red,
+	i = colors.blue,
+	v = colors.green,
+	[""] = colors.blue,
+	V = colors.blue,
+	c = colors.magenta,
+	no = colors.red,
+	s = colors.orange,
+	S = colors.orange,
+	[""] = colors.orange,
+	ic = colors.yellow,
+	R = colors.violet,
+	Rv = colors.violet,
+	cv = colors.red,
+	ce = colors.red,
+	r = colors.cyan,
+	rm = colors.cyan,
+	["r?"] = colors.cyan,
+	["!"] = colors.red,
+	t = colors.red,
+}
 
 local config = {
 	options = {
