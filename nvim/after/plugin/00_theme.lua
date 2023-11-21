40696cc0882c15de86af09f4c1b2b3f05a5842a1
Kia Shakiba <kiashakiba@protonmail.com>
2023-11-21 02:39:13 -0500

    Update gitsigns colours

--- nvim/after/plugin/00_theme.lua
+++ nvim/after/plugin/00_theme.lua
@@ -4,6 +4,14 @@ local palette = monokai.soda
 monokai.setup {
 	palette,
 	custom_hlgroups = {
+		GitSignsAdd = {
+			fg = palette.green,
+			bg = palette.base2
+		},
+		GitSignsDelete = {
+			fg = palette.red,
+			bg = palette.base2
+		},
 		GitSignsChange = {
 			fg = palette.orange,
 			bg = palette.base2
